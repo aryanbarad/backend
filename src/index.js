b2851039bf9b8592144e0513fd893267497b042a
barad aryan <aryanbarad94@gmail.com>
2025-08-23 01:44:50 +0530

    backend connecton with database

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ dotenv.config({ path: './env' })
 
 
 connectDB()
-
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => { 
+            console.log(`server is runing on PORT ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("mogoDB connection faild!!", error)
+    })
 
 
 //      (async () => {
