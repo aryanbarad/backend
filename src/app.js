027e9f18714a0e73bbd058f01fc1f7e7a83acc7e
barad aryan <aryanbarad94@gmail.com>
2025-08-28 00:24:33 +0530

    controller and routes updeted

--- src/app.js
+++ src/app.js
@@ -12,5 +12,9 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
+//routes import
+
+
+
 app.use(cookieparser())
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import dotenv from "dotenv"
 import express from "express"
 import connectDB from "./db/index.js"
-dotenv.config({ path: './env' })
+dotenv.config({ path: "./env " })
 
 const app = express()
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 8000, () => { 
-            console.log(`server is runing on PORT ${process.env.PORT}`)
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is runing on PORT ${process.env.PORT
+
+            }`)
         })
     })
     .catch((error) => {
@@ -66,7 +68,7 @@ connectDB()
 
 
 
-    
+
 //      (async () => {
 //         try {
 //             await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
